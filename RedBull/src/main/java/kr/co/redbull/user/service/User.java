379fcfca451960dd50f09cbb0c2c251d404d4df6
sist <sist@DESKTOP-IR0HIIQ>
2019-09-27 08:55:56 +0900

    user vo 수정 / impl 등록 메소드 작성

--- RedBull/src/main/java/kr/co/redbull/user/service/User.java
+++ RedBull/src/main/java/kr/co/redbull/user/service/User.java
@@ -19,88 +19,212 @@ public class User extends DTO {
 	private String regdt;  // 등록일(SYSDATE)
 	// totalCnt, num 상속받음
 	
+	/**기본생성자*/
+	public User() {
+
+	}
 	
+	/**멤버변수 전체를 매개변수로 하는 생성자*/
+	public User(String rid, String passwd, String uname, String birth, String phone, int postnum, String address,
+			String detadd, int lvl, int upoint, int inum, String regdt) {
+		super();
+		this.rid = rid;
+		this.passwd = passwd;
+		this.uname = uname;
+		this.birth = birth;
+		this.phone = phone;
+		this.postnum = postnum;
+		this.address = address;
+		this.detadd = detadd;
+		this.lvl = lvl;
+		this.upoint = upoint;
+		this.inum = inum;
+		this.regdt = regdt;
+	}
+
 	/**setter/getter*/
 	public String getRid() {
 		return rid;
 	}
+
 	public void setRid(String rid) {
 		this.rid = rid;
 	}
+
 	public String getPasswd() {
 		return passwd;
 	}
+
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
 	}
+
 	public String getUname() {
 		return uname;
 	}
+
 	public void setUname(String uname) {
 		this.uname = uname;
 	}
+
 	public String getBirth() {
 		return birth;
 	}
+
 	public void setBirth(String birth) {
 		this.birth = birth;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public int getPostnum() {
 		return postnum;
 	}
+
 	public void setPostnum(int postnum) {
 		this.postnum = postnum;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getDetadd() {
 		return detadd;
 	}
+
 	public void setDetadd(String detadd) {
 		this.detadd = detadd;
 	}
+
 	public int getLvl() {
 		return lvl;
 	}
+
 	public void setLvl(int lvl) {
 		this.lvl = lvl;
 	}
+
 	public int getUpoint() {
 		return upoint;
 	}
+
 	public void setUpoint(int upoint) {
 		this.upoint = upoint;
 	}
+
 	public int getInum() {
 		return inum;
 	}
+
 	public void setInum(int inum) {
 		this.inum = inum;
 	}
+
 	public String getRegdt() {
 		return regdt;
 	}
+
 	public void setRegdt(String regdt) {
 		this.regdt = regdt;
 	}
-	
-	/**toString 재정의*/
+
+	/**toString() 재정의*/
 	@Override
 	public String toString() {
 		return "User [rid=" + rid + ", passwd=" + passwd + ", uname=" + uname + ", birth=" + birth + ", phone=" + phone
 				+ ", postnum=" + postnum + ", address=" + address + ", detadd=" + detadd + ", lvl=" + lvl + ", upoint="
 				+ upoint + ", inum=" + inum + ", regdt=" + regdt + ", toString()=" + super.toString() + "]";
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((birth == null) ? 0 : birth.hashCode());
+		result = prime * result + ((detadd == null) ? 0 : detadd.hashCode());
+		result = prime * result + inum;
+		result = prime * result + lvl;
+		result = prime * result + ((passwd == null) ? 0 : passwd.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		result = prime * result + postnum;
+		result = prime * result + ((regdt == null) ? 0 : regdt.hashCode());
+		result = prime * result + ((rid == null) ? 0 : rid.hashCode());
+		result = prime * result + ((uname == null) ? 0 : uname.hashCode());
+		result = prime * result + upoint;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (birth == null) {
+			if (other.birth != null)
+				return false;
+		} else if (!birth.equals(other.birth))
+			return false;
+		if (detadd == null) {
+			if (other.detadd != null)
+				return false;
+		} else if (!detadd.equals(other.detadd))
+			return false;
+		if (inum != other.inum)
+			return false;
+		if (lvl != other.lvl)
+			return false;
+		if (passwd == null) {
+			if (other.passwd != null)
+				return false;
+		} else if (!passwd.equals(other.passwd))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		if (postnum != other.postnum)
+			return false;
+		if (regdt == null) {
+			if (other.regdt != null)
+				return false;
+		} else if (!regdt.equals(other.regdt))
+			return false;
+		if (rid == null) {
+			if (other.rid != null)
+				return false;
+		} else if (!rid.equals(other.rid))
+			return false;
+		if (uname == null) {
+			if (other.uname != null)
+				return false;
+		} else if (!uname.equals(other.uname))
+			return false;
+		if (upoint != other.upoint)
+			return false;
+		return true;
+	}
+
 
 }//--class
--- RedBull/src/main/java/kr/co/redbull/user/service/impl/UserDaoImpl.java
+++ RedBull/src/main/java/kr/co/redbull/user/service/impl/UserDaoImpl.java
@@ -2,12 +2,15 @@ package kr.co.redbull.user.service.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.co.redbull.cmn.DTO;
 import kr.co.redbull.cmn.WorkDiv;
+import kr.co.redbull.user.service.User;
 
 /**회원관리 Dao*/
 @Repository
@@ -15,7 +18,11 @@ public class UserDaoImpl implements WorkDiv {
 	
 	Logger LOG = LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate; // mybatis
 	
+	private final String NAMESPACE = "kr.co.redbull.user"; // 쿼리찾을 때 필요
+		
 
 	/**회원정보 수정*/
 	@Override
@@ -34,8 +41,25 @@ public class UserDaoImpl implements WorkDiv {
 	/**회원 가입(회원정보 등록)*/
 	@Override
 	public int do_save(DTO dto) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		String statement = this.NAMESPACE + ".do_save"; // kr.co.redbull.user.do_save
+		
+		User user = (User) dto;
+		
+		LOG.debug("=============================");
+		LOG.debug("01. statement="+statement);
+		LOG.debug("=============================");			
+		
+		LOG.debug("=============================");
+		LOG.debug("02. param="+user);
+		LOG.debug("=============================");	
+		
+		int flag = this.sqlSessionTemplate.insert(statement, user);
+		LOG.debug("=============================");
+		LOG.debug("03. flag="+flag);
+		LOG.debug("=============================");	
+		
+		return flag;
 	}
 
 	/**비밀번호 찾기(회원정보 단건 조회)*/
