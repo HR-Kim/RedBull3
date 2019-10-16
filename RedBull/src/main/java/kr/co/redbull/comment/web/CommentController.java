111158a215ed25bfd6bfe8b41e6aa7142c8ec26d
sist <sist@DESKTOP-GASVUS9>
2019-10-16 09:34:49 +0900

    commit

--- RedBull/src/main/java/kr/co/redbull/comment/web/CommentController.java
+++ RedBull/src/main/java/kr/co/redbull/comment/web/CommentController.java
@@ -1,9 +1,14 @@
 package kr.co.redbull.comment.web;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.google.gson.Gson;
 
 import kr.co.redbull.cmn.Message;
+import kr.co.redbull.cmn.Search;
 import kr.co.redbull.comment.service.Comment;
 import kr.co.redbull.comment.service.CommentService;
 
@@ -24,38 +30,31 @@ public class CommentController {
 	
 	private String viewDetailNm ="board/board_detail";
 	
-//	@RequestMapping(value = "comment/get_retrieve.do", method = RequestMethod.GET)
-//	public String get_retrieve(Search search, Model model, HttpServletRequest request) {
-//		
-//		LOG.debug("================================");
-//		LOG.debug("search:"+search);
-//		LOG.debug("================================");
-//		
-//		if(search.getPageSize()==0) search.setPageSize(10);
-//		if(search.getPageNum()==0)  search.setPageNum(1);
-//		
-//		
-//		model.addAttribute("vo", search);
-//		
-//		List<Board> list = (List<Board>)commentService.get_retrieve(search);
-//		
-//		model.addAttribute("list", list);
-//		
-//		//총건수
-//		int totalCnt = 0;
-//		if(null!=list && list.size()>0) {
-//			totalCnt = list.get(0).getTotalCnt();
-//			model.addAttribute("totalCnt", totalCnt);
-//		}
+	@RequestMapping(value = "comment/get_retrieve.do", method = RequestMethod.GET)
+	public String get_retrieve(Search search, Model model, HttpServletRequest request) {
 		
-//		if(search.getSearchDiv().equals("10")) {
-//			viewListNm = "board/notice_list";
-//		}else if(search.getSearchDiv().equals("20")) {
-//			viewListNm = "board/question_list";
-//		}
-//		
-//		return viewDetailNm;
-//	}
+		LOG.debug("================================");
+		LOG.debug("search:"+search);
+		LOG.debug("================================");
+		
+		if(search.getPageSize()==0) search.setPageSize(10);
+		if(search.getPageNum()==0)  search.setPageNum(1);
+		
+		model.addAttribute("searchVO", search);
+		
+		List<Comment> list = (List<Comment>)commentService.get_retrieve(search);
+		
+		model.addAttribute("list", list);
+		
+		//총건수
+		int totalCnt = 0;
+		if(null!=list && list.size()>0) {
+			totalCnt = list.get(0).getTotalCnt();
+			model.addAttribute("totalCnt", totalCnt);
+		}
+		
+		return viewDetailNm;
+	}
 	
 //	@RequestMapping(value = "board/get_selectOne.do", method = RequestMethod.GET)
 //	public String get_selectOne(Board board, Model model) {
@@ -87,35 +86,35 @@ public class CommentController {
 //		return viewDetailNm;
 //	}
 //	
-//	@RequestMapping(value = "board/do_update.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-//	@ResponseBody
-//	public String do_update(Board board) {
-//		
-//		LOG.debug("================================");
-//		LOG.debug("board:"+board);
-//		LOG.debug("================================");
-//		
-//		int flag = commentService.do_update(board);
-//		
-//		Message message = new Message();
-//		
-//		if(flag>0) {
-//			message.setMsgId(String.valueOf(flag));
-//			message.setMsgMsg("수정되었습니다.");
-//		}else {
-//			message.setMsgId(String.valueOf(flag));
-//			message.setMsgMsg("수정실패");
-//		}
-//		
-//		Gson gson = new Gson();
-//		String gsonStr = gson.toJson(message);
-//		
-//		LOG.debug("================================");
-//		LOG.debug("gsonStr:"+gsonStr);
-//		LOG.debug("================================");
-//		
-//		return gsonStr;
-//	}
+	@RequestMapping(value = "comment/do_update.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String do_update(Comment comment) {
+		
+		LOG.debug("================================");
+		LOG.debug("comment:"+comment);
+		LOG.debug("================================");
+		
+		int flag = commentService.do_update(comment);
+		
+		Message message = new Message();
+		
+		if(flag>0) {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("수정되었습니다.");
+		}else {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("수정실패");
+		}
+		
+		Gson gson = new Gson();
+		String gsonStr = gson.toJson(message);
+		
+		LOG.debug("================================");
+		LOG.debug("gsonStr:"+gsonStr);
+		LOG.debug("================================");
+		
+		return gsonStr;
+	}
 	
 	@RequestMapping(value = "comment/do_save.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 	@ResponseBody
@@ -147,33 +146,33 @@ public class CommentController {
 		return gsonStr;
 	}
 	
-//	@RequestMapping(value = "board/do_delete.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-//	@ResponseBody
-//	public String do_delete(Board board) {
-//		
-//		LOG.debug("================================");
-//		LOG.debug("board:"+board);
-//		LOG.debug("================================");
-//		
-//		int flag = commentService.do_delete(board);
-//		
-//		Message message = new Message();
-//		
-//		if(flag>0) {
-//			message.setMsgId(String.valueOf(flag));
-//			message.setMsgMsg("삭제되었습니다.");
-//		}else {
-//			message.setMsgId(String.valueOf(flag));
-//			message.setMsgMsg("삭제실패");
-//		}
-//		
-//		Gson gson = new Gson();
-//		String gsonStr = gson.toJson(message);
-//		
-//		LOG.debug("================================");
-//		LOG.debug("gsonStr:"+gsonStr);
-//		LOG.debug("================================");
-//		
-//		return gsonStr;
-//	}
+	@RequestMapping(value = "comment/do_delete.do", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String do_delete(Comment comment) {
+		
+		LOG.debug("================================");
+		LOG.debug("comment:"+comment);
+		LOG.debug("================================");
+		
+		int flag = commentService.do_delete(comment);
+		
+		Message message = new Message();
+		
+		if(flag>0) {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("삭제되었습니다.");
+		}else {
+			message.setMsgId(String.valueOf(flag));
+			message.setMsgMsg("삭제실패");
+		}
+		
+		Gson gson = new Gson();
+		String gsonStr = gson.toJson(message);
+		
+		LOG.debug("================================");
+		LOG.debug("gsonStr:"+gsonStr);
+		LOG.debug("================================");
+		
+		return gsonStr;
+	}
 }
